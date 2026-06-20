7a47ae293a23f4205eaddc6ab06e0fb69b8ae6ce
Mardyon Yongson <sj2.myongson26@joysistvi.edu.ph>
2026-06-20 17:55:05 +0800

    Add discussion code for day2 and day3

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2.discussion; //package
+
+
+/**
+ *
+ * @author Mardyon
+ */
+public class SampleCode { // sample class
+    
+    public static void main(String[] args) { // main method
+        System.out.println("Hello World!");    
+    }
+    
+    //Main Method  (psvm + tab) -> starting point of programs execution
+    //Print Statement (sout + tab) -> it tells the computer to print something
+    
+    /*
+        2 Types of Comments
+        1. Single Line Comment
+        2. Multi Line Comment
+    */
+    
+}
